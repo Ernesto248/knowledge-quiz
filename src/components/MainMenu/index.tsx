3ed166b49571ef4ad925ea96c1b4a0ad633e8cd6
Ernesto248 <ernestoleonard8@gmail.com>
2025-02-09 01:57:40 -0500

    Fixed th amount of questions enum mapping

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -1,61 +1,59 @@
-import { DifficultyType, QuestionAmountType } from "../../types/types.d"
+import { DifficultyType, QuestionAmountType } from "../../types/types.d";
 
 interface Props {
-    onClickDifficulty: (difficulty:DifficultyType)=>void
-    onClickQuestionAmount: (questionAmount:QuestionAmountType)=>void
-    startQuiz:()=>void
+  onClickDifficulty: (difficulty: DifficultyType) => void;
+  onClickQuestionAmount: (questionAmount: QuestionAmountType) => void;
+  startQuiz: () => void;
 }
-const MainMenu = ({onClickDifficulty, onClickQuestionAmount, startQuiz}:Props) => {
-
-    const handleDifficultyClick = (e:React.MouseEvent<HTMLButtonElement>)=>{
-        const value = e.currentTarget.value as DifficultyType
-        if(!Object.values(DifficultyType).includes(value)){
-            console.log("mal mi pana");
-            
-            return;
-        }
-        onClickDifficulty(value)
+const MainMenu = ({
+  onClickDifficulty,
+  onClickQuestionAmount,
+  startQuiz,
+}: Props) => {
+  const handleDifficultyClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const value = e.currentTarget.value as DifficultyType;
+    if (!Object.values(DifficultyType).includes(value)) {
+      console.log("mal mi pana");
+      return;
     }
+    onClickDifficulty(value);
+  };
 
-    const handleQuestionAmountClick = (e:React.MouseEvent<HTMLButtonElement>)=>{
-        const value = Number(e.currentTarget.value) as QuestionAmountType
-        if(!Object.values(QuestionAmountType).includes(value)){
-            console.log("mal mi pana");
-            return;
-        }
-        onClickQuestionAmount(value)
+  const handleQuestionAmountClick = (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const value = Number(e.currentTarget.value) as QuestionAmountType;
+    if (!Object.values(QuestionAmountType).includes(value)) {
+      console.log("mal mi pana");
+      return;
     }
+    onClickQuestionAmount(value);
+  };
 
   return (
     <section>
-        <button onClick={startQuiz}>Start Quiz</button>
-        <ul className="flex flex-row gap-4">
-            <li>
-                <button value={DifficultyType.Easy} onClick={handleDifficultyClick}>Easy</button>    
-            </li>
-            <li>
-                <button value={DifficultyType.Medium} onClick={handleDifficultyClick}>Medium</button>
-            </li>
-            <li>
-                <button value={DifficultyType.Hard} onClick={handleDifficultyClick}>Hard</button>
-            </li>
-        </ul>
-        <ul className="flex flex-row gap-4">
-            <li>
-                <button value={QuestionAmountType.Five} onClick={handleQuestionAmountClick}>5</button>
-            </li>
-            <li>
-                <button value={QuestionAmountType.Ten} onClick={handleQuestionAmountClick}>10</button>
-            </li>
-            <li>
-                <button value={QuestionAmountType.Fifteen} onClick={handleQuestionAmountClick}>15</button>
-            </li>
-            <li>
-                <button value={QuestionAmountType.Twenty} onClick={handleQuestionAmountClick}>20</button>
-            </li>
-        </ul>
+      <button onClick={startQuiz}>Start Quiz</button>
+      <ul className="flex flex-row gap-4">
+        {Object.values(DifficultyType).map((difficulty) => (
+          <li key={difficulty}>
+            <button value={difficulty} onClick={handleDifficultyClick}>
+              {difficulty}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <ul className="flex flex-row gap-4">
+        {/* Object.values(QuestionAmountType) returns a bidirectional object with keys and values */}
+        {[5, 10, 15, 20].map((amount) => (
+          <li key={amount}>
+            <button value={amount} onClick={handleQuestionAmountClick}>
+              {amount}
+            </button>
+          </li>
+        ))}
+      </ul>
     </section>
-  )
-}
+  );
+};
 
-export default MainMenu
+export default MainMenu;
